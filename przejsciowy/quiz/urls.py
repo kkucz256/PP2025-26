bfaea60b08abfe7a803259433f9e878f69e9900f
Ipper18 <64928480+Ipper18@users.noreply.github.com>
2025-12-11 17:52:26 +0100

    wersja_menu_v0.3

--- przejsciowy/quiz/urls.py
+++ przejsciowy/quiz/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from . import views
- 
+
+app_name = 'quiz'
+
 urlpatterns = [
-    path('upload/', views.upload_pdf_view, name='upload_pdf_view')
+    path('', views.main_menu_view, name='main_menu'),
+    path('start-quiz/', views.start_quiz_view, name='start_quiz'),
+    path('my-notes/', views.my_notes_view, name='my_notes'),
+    path('my-notes/upload/', views.upload_pdf_view, name='upload_pdf_view'),
+    path('my-quizzes/', views.my_quizzes_view, name='my_quizzes'),
+    path('multiplayer/', views.multiplayer_view, name='multiplayer'),
+    path('statistics/', views.statistics_view, name='statistics'),
 ]
