2b7ac033f8e5fa8a27f6d2cfc6016dd0afc27c68
JNielacny <naczelnyh8r@gmail.com>
2025-12-15 19:28:27 +0100

    Dodanie rejestracji, logowania oraz opcji wylogowywania. Zmiany na dynamiczne wartosci w widokach, przerywanie sesji wraz z wyłączaniem serwera.

--- przejsciowy/quiz/urls.py
+++ przejsciowy/quiz/urls.py
@@ -5,6 +5,9 @@ app_name = 'quiz'
 
 urlpatterns = [
     path('', views.main_menu_view, name='main_menu'),
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
     path('start-quiz/', views.start_quiz_view, name='start_quiz'),
     path('my-notes/', views.my_notes_view, name='my_notes'),
     path('my-notes/upload/', views.upload_pdf_view, name='upload_pdf_view'),
